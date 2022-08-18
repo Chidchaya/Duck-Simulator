aa43c543e88cb92d8ec43d93b1e91c40d23e6f6e
Chidchaya Jenkalpana <chidchaya.j@ku.th>
2022-08-18 22:19:27 +0700

    Duck Simulator

---
+++ src/QuackCounter.java
@@ -0,0 +1,17 @@
+public class QuackCounter implements Quackable {
+    Quackable duck;
+    static int numberOfQuack;
+
+    public QuackCounter (Quackable duck){
+        this.duck = duck;
+    }
+
+    @Override
+    public void quack() {
+        duck.quack();
+        numberOfQuack++;
+    }
+    public static int getQuacks(){
+        return numberOfQuack;
+    }
+}
